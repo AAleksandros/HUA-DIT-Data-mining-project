cbdb0c1732328a65b9dc96d9bc5265ad971b2100
Michael Kinnas <michaelkinnas@gmail.com>
2023-12-29 21:27:12 +0200

    dataset sampling techniques

--- helper_functions.py
+++ helper_functions.py
@@ -6,14 +6,18 @@ import torch
 import numpy as np
 
 
-def plot_roc_curves(best_a, best_b, best_c, title):
+def plot_roc_curves(best_a, best_b, best_c, best_d, title):
     fpr_a, tpr_a, thresholds = roc_curve(best_a['true'].flatten(), best_a['preds'].flatten())
     fpr_b, tpr_b, thresholds = roc_curve(best_b['true'].flatten(), best_b['preds'].flatten())
     fpr_c, tpr_c, thresholds = roc_curve(best_c['true'].flatten(), best_c['preds'].flatten())
+    fpr_d, tpr_d, thresholds = roc_curve(best_d['true'].flatten(), best_d['preds'].flatten())
+    sns.reset_orig()
+    sns.set_theme()
 
     plt.plot(fpr_a, tpr_a, label=f'{best_a["label"]} AUC: {auc(fpr_a, tpr_a):.2}')
     plt.plot(fpr_b, tpr_b, label=f'{best_b["label"]} AUC: {auc(fpr_b, tpr_b):.2}')
     plt.plot(fpr_c, tpr_c, label=f'{best_c["label"]} AUC: {auc(fpr_c, tpr_c):.2}')
+    plt.plot(fpr_d, tpr_d, label=f'{best_d["label"]} AUC: {auc(fpr_d, tpr_d):.2}')
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
     plt.title(title)
@@ -100,15 +104,16 @@ def avg_classification_reports(reports, title):
     # report_b = classification_report(torch.round(torch.tensor(best_b['true'])), torch.round(torch.tensor(best_b['preds'])), output_dict=True, target_names=['0. non-oscar', '1. oscar'])
     # report_c = classification_report(torch.round(torch.tensor(best_c['true'])), torch.round(torch.tensor(best_c['preds'])), output_dict=True, target_names=['0. non-oscar', '1. oscar'])
 
-    fig, (ax1, ax2, ax3) = plt.subplots(1,3)  
-    
+    fig, axes = plt.subplots(nrows=2, ncols=2)
     sns.reset_orig()
     sns.set_theme()
-    sns.set(rc={'figure.figsize':(16,4)})
+    sns.set(rc={'figure.figsize':(8, 6)})
 
-    sns.heatmap(pd.DataFrame(avg_reports[0]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=ax1, xticklabels=True, yticklabels=True, cbar=True).set_title(avg_reports[0]['label'])
-    sns.heatmap(pd.DataFrame(avg_reports[1]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=ax2, xticklabels=True, yticklabels=False, cbar=True).set_title(avg_reports[1]['label'])
-    sns.heatmap(pd.DataFrame(avg_reports[2]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=ax3, xticklabels=True, yticklabels=False, cbar=True).set_title(avg_reports[2]['label'])
+
+    sns.heatmap(pd.DataFrame(avg_reports[0]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[0][0], xticklabels=False, yticklabels=True, cbar=True).set_title(avg_reports[0]['label'])
+    sns.heatmap(pd.DataFrame(avg_reports[1]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[0][1], xticklabels=False, yticklabels=False, cbar=True).set_title(avg_reports[1]['label'])
+    sns.heatmap(pd.DataFrame(avg_reports[2]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[1][0], xticklabels=True, yticklabels=True, cbar=True).set_title(avg_reports[2]['label'])
+    sns.heatmap(pd.DataFrame(avg_reports[3]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[1][1], xticklabels=True, yticklabels=False, cbar=True).set_title(avg_reports[3]['label'])
     plt.suptitle(title)
 
     plt.show()
