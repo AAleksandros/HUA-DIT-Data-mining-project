4b720feea4bf0aef8bc8881f422611c6e4768ca8
Michael Kinnas <michaelkinnas@gmail.com>
2024-01-14 16:44:41 +0200

    classification_refactoring

---
+++ classifiers.py
@@ -0,0 +1,241 @@
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.metrics import classification_report, silhouette_score, davies_bouldin_score, calinski_harabasz_score, confusion_matrix, roc_curve, auc, RocCurveDisplay, ConfusionMatrixDisplay
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+from sklearn.cluster import KMeans, DBSCAN, HDBSCAN, AgglomerativeClustering
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.utils import resample
+from sklearn.naive_bayes import ComplementNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+import numpy as np
+
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+
+
+def build_resampled_datasets(dataset):
+    # Separate testing split from dataset
+    df_train, df_test = train_test_split(dataset, test_size=0.2, stratify=dataset['oscar_winners'])
+
+    # Separate classes
+    df_majority = df_train[(df_train['oscar_winners']==0)].reset_index().drop('index', axis=1)
+    df_minority = df_train[(df_train['oscar_winners']==1)].reset_index().drop('index', axis=1)
+
+    # Upsample the minority class of the dataset
+    df_minority_upsampled = resample(df_minority, replace=True, n_samples= len(df_majority), random_state=42)
+    df_minority_upsampled = df_minority_upsampled.reset_index().drop('index', axis=1)
+    df_upsampled = pd.concat([df_minority_upsampled, df_majority]).sort_index(kind='merge')
+    df_upsampled = df_upsampled.reset_index().drop('index', axis=1)
+
+    # Downsample the majority class of the dataset
+    df_majority_downsampled = resample(df_majority, replace=True, n_samples= len(df_minority), random_state=42)
+    df_majority_downsampled = df_majority_downsampled.reset_index().drop('index', axis=1)
+    df_downsampled = pd.concat([df_majority_downsampled, df_minority]).sort_index(kind='merge')
+    df_downsampled = df_downsampled.reset_index().drop('index', axis=1)
+
+    # Resample using SMOTE method
+    sm = SMOTE(sampling_strategy='minority', random_state=42)
+    df_smote_X, df_smote_Y = sm.fit_resample(df_train.drop('oscar_winners', axis=1), df_train['oscar_winners'])
+    df_smote = pd.concat([pd.DataFrame(df_smote_X), pd.DataFrame(df_smote_Y)], axis=1)
+    
+    # Dataframes are returned
+    return {
+        'default' : dataset,
+        'upsampled' : df_upsampled,
+        'downsampled' : df_downsampled,
+        'SMOTE' : df_smote
+    }, df_test
+
+
+def k_fold_fit_and_test(model, datasets, test, param=None):
+    results = {}    
+
+
+    for sampling, dataset in zip(datasets.keys(), datasets.values()):
+        # Prepare dataset for training k-fold splits
+
+        results[sampling] = []
+
+        X = dataset.drop(['oscar_winners'], axis=1)
+        y = dataset[['oscar_winners']]
+
+        # Create k-fold splits and stratify classes
+        kf = StratifiedKFold(n_splits=5, shuffle=False)
+
+        for i, train_idx in enumerate(kf.split(X, y), 1):
+
+            train = dataset.iloc[train_idx[1]] 
+            
+            # Separate labels
+            X_train, y_train = train.drop(['oscar_winners'], axis=1).values, train[['oscar_winners']].values
+            y_train = y_train.squeeze(axis=1)
+
+            model.fit(X_train, y_train)
+            
+            # Test on test split
+            X_test, y_test = test.drop(['oscar_winners'], axis=1).values, test[['oscar_winners']].values
+            y_test = y_test.squeeze(axis=1)
+
+            y_pred = model.predict(X_test)             
+            
+            # y_pred = [float(x) for x in y_pred]
+            # y_test = [float(x) for x in y_test]
+            report = classification_report(y_test, y_pred, output_dict=True, zero_division=0.0)
+
+            print(f"Fold {i} 1-F1 score: {report['1']['f1-score']:.4f} for {sampling}")
+           
+        # best_performing_knn_sampling = max(reports, key=lambda a:a['report']['1.0']['f1-score'])
+
+            results[sampling].append({
+                'fold' : str(i),
+                'report' : report,
+                'preds' : y_pred,
+                'true' : y_test
+            })
+
+    return results
+
+def k_fold_fit_and_test_knn(datasets, test, param=None):
+    results = {}    
+
+    for sampling, dataset in zip(datasets.keys(), datasets.values()):
+
+        # Find best k parameter
+        k_values = [i for i in range (2, 30)] 
+        scores = []        
+
+        for k in k_values:
+            knn = KNeighborsClassifier(n_neighbors=k)
+            score = cross_val_score(knn, dataset.drop(['oscar_winners'], axis=1), dataset['oscar_winners'], cv=5)
+            scores.append(np.mean(score))
+        
+        best_index = np.argmax(scores)
+        best_k = k_values[best_index]
+
+        # Prepare dataset for training k-fold splits
+
+        results[sampling] = []
+        
+
+        X = dataset.drop(['oscar_winners'], axis=1)
+        y = dataset[['oscar_winners']]
+
+
+        # Create k-fold splits and stratify classes
+        kf = StratifiedKFold(n_splits=5, shuffle=False)
+
+        for i, train_idx in enumerate(kf.split(X, y), 1):
+
+            train = dataset.iloc[train_idx[1]] 
+            
+            # Separate labels
+            X_train, y_train = train.drop(['oscar_winners'], axis=1).values, train[['oscar_winners']].values
+            y_train = y_train.squeeze(axis=1)
+
+            model = KNeighborsClassifier(n_neighbors=best_k)
+            model.fit(X_train, y_train)
+            
+            # Test on test split
+            X_test, y_test = test.drop(['oscar_winners'], axis=1).values, test[['oscar_winners']].values
+            y_test = y_test.squeeze(axis=1)
+
+            y_pred = model.predict(X_test)             
+            
+            # y_pred = [float(x) for x in y_pred]
+            # y_test = [float(x) for x in y_test]
+            report = classification_report(y_test, y_pred, output_dict=True, zero_division=0.0)
+
+            print(f"Fold {i} 1-F1 score: {report['1']['f1-score']:.4f} for {sampling}")
+           
+        # best_performing_knn_sampling = max(reports, key=lambda a:a['report']['1.0']['f1-score'])
+
+            results[sampling].append({
+                'fold' : str(i),
+                'report' : report,
+                'preds' : y_pred,
+                'true' : y_test,
+                'k' : best_k
+            })
+
+    return results
+
+
+def k_fold_fit_and_test_rf(datasets, test, param=None):
+    results = {}    
+
+    for sampling, dataset in zip(datasets.keys(), datasets.values()):
+
+        # Find best k parameter
+        k_values = [i for i in range (2, 15)] 
+        scores = []
+
+        
+
+        for k in k_values:
+            knn = KNeighborsClassifier(n_neighbors=k)
+            score = cross_val_score(knn, dataset.drop(['oscar_winners'], axis=1), dataset['oscar_winners'], cv=5)
+            scores.append(np.mean(score))
+
+        
+        best_index = np.argmax(scores)
+        best_k = k_values[best_index]
+
+        # Prepare dataset for training k-fold splits
+
+        results[sampling] = []
+        
+
+        X = dataset.drop(['oscar_winners'], axis=1)
+        y = dataset[['oscar_winners']]
+
+
+        # Create k-fold splits and stratify classes
+        kf = StratifiedKFold(n_splits=5, shuffle=False)
+
+        for i, train_idx in enumerate(kf.split(X, y), 1):
+
+            train = dataset.iloc[train_idx[1]] 
+            
+            # Separate labels
+            X_train, y_train = train.drop(['oscar_winners'], axis=1).values, train[['oscar_winners']].values
+            y_train = y_train.squeeze(axis=1)
+
+            model = RandomForestClassifier(max_depth=best_k)
+            model.fit(X_train, y_train)
+            
+            # Test on test split
+            X_test, y_test = test.drop(['oscar_winners'], axis=1).values, test[['oscar_winners']].values
+            y_test = y_test.squeeze(axis=1)
+
+            y_pred = model.predict(X_test)             
+            
+            # y_pred = [float(x) for x in y_pred]
+            # y_test = [float(x) for x in y_test]
+            report = classification_report(y_test, y_pred, output_dict=True, zero_division=0.0)
+
+            print(f"Fold {i} 1-F1 score: {report['1']['f1-score']:.4f} for {sampling}")
+           
+        # best_performing_knn_sampling = max(reports, key=lambda a:a['report']['1.0']['f1-score'])
+
+            results[sampling].append({
+                'fold' : str(i),
+                'report' : report,
+                'preds' : y_pred,
+                'true' : y_test,
+                'k' : best_k
+            })
+
+    return results
+
+def best_f1_score_for_each_sampling_method(results, model):
+    best = {}
+    best['default'] = max(results[model]['default'], key=lambda a:a['report']['1']['f1-score'])
+    best['upsampled'] = max(results[model]['upsampled'], key=lambda a:a['report']['1']['f1-score'])
+    best['downsampled'] = max(results[model]['downsampled'], key=lambda a:a['report']['1']['f1-score'])
+    best['SMOTE'] = max(results[model]['SMOTE'], key=lambda a:a['report']['1']['f1-score'])
+    return best
--- plotting_functions.py
+++ plotting_functions.py
@@ -7,56 +7,54 @@ import torch
 import numpy as np
 
 
-def plot_roc_curves(best_a, best_b, best_c, best_d, title):
-    fpr_a, tpr_a, thresholds = roc_curve(best_a['true'], best_a['preds'])
-    fpr_b, tpr_b, thresholds = roc_curve(best_b['true'], best_b['preds'])
-    fpr_c, tpr_c, thresholds = roc_curve(best_c['true'], best_c['preds'])
-    fpr_d, tpr_d, thresholds = roc_curve(best_d['true'], best_d['preds'])
+def plot_roc_curves(results, title):
     sns.reset_orig()
     sns.set_theme()
     plt.figure(figsize=(8, 6))
-    plt.plot(fpr_a, tpr_a, label=f'{best_a["label"]} AUC: {auc(fpr_a, tpr_a):.2}')
-    plt.plot(fpr_b, tpr_b, label=f'{best_b["label"]} AUC: {auc(fpr_b, tpr_b):.2}')
-    plt.plot(fpr_c, tpr_c, label=f'{best_c["label"]} AUC: {auc(fpr_c, tpr_c):.2}')
-    plt.plot(fpr_d, tpr_d, label=f'{best_d["label"]} AUC: {auc(fpr_d, tpr_d):.2}')
+    
+    for key, result in zip(results.keys(), results.values()):
+        fpr, tpr, thr = roc_curve(result['true'], result['preds'])
+        plt.plot(fpr, tpr, label=f'{key} AUC: {auc(fpr, tpr):.2}')
+
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
     
-    plt.title(title)
+    plt.title(f"{title} ROC curves")
     plt.legend()
     plt.show()
-    
-    
-def plot_classification_reports_averages(reports, title):
+
+
+def plot_classification_reports_averages(reports, name):
     avg_reports = []
-    for report in reports:
+    model = reports[name]
+    for technique in model:
         avg_reports.append({
-            'label' : report[0]['label'],
+            'sampling' : technique,
             'report' : {
-                '0.0' : {
-                    'precision' :  np.average([x['report']['0.0']['precision'] for x in report]),
-                    'recall': np.average([x['report']['0.0']['recall'] for x in report]),
-                    'f1-score' : np.average([x['report']['0.0']['f1-score'] for x in report]),
-                    'support': np.average([x['report']['0.0']['support'] for x in report]),
+                '0' : {
+                    'precision' : np.average([x['report']['0']['precision'] for x in model[technique]]),
+                    'recall': np.average([x['report']['0']['recall'] for x in model[technique]]),
+                    'f1-score' : np.average([x['report']['0']['f1-score'] for x in model[technique]]),
+                    'support': np.average([x['report']['0']['support'] for x in model[technique]]),
                 },
-                '1.0' : {
-                    'precision' :  np.average([x['report']['1.0']['precision'] for x in report]),
-                    'recall': np.average([x['report']['1.0']['recall'] for x in report]),
-                    'f1-score' : np.average([x['report']['1.0']['f1-score'] for x in report]),
-                    'support': np.average([x['report']['1.0']['support'] for x in report]),
+                '1' : {
+                    'precision' :  np.average([x['report']['1']['precision'] for x in model[technique]]),
+                    'recall': np.average([x['report']['1']['recall'] for x in model[technique]]),
+                    'f1-score' : np.average([x['report']['1']['f1-score'] for x in model[technique]]),
+                    'support': np.average([x['report']['1']['support'] for x in model[technique]]),
                 },
-                'accuracy' : np.average([x['report']['accuracy'] for x in report]),
+                'accuracy' : np.average([x['report']['accuracy'] for x in model[technique]]),
                 'macro avg': {
-                    'precision' : np.average([x['report']['macro avg']['precision'] for x in report]),
-                    'recall' : np.average([x['report']['macro avg']['recall'] for x in report]),
-                    'f1-score' : np.average([x['report']['macro avg']['f1-score'] for x in report]),
-                    'support' : np.average([x['report']['macro avg']['support'] for x in report]),
+                    'precision' : np.average([x['report']['macro avg']['precision'] for x in model[technique]]),
+                    'recall' : np.average([x['report']['macro avg']['recall'] for x in model[technique]]),
+                    'f1-score' : np.average([x['report']['macro avg']['f1-score'] for x in model[technique]]),
+                    'support' : np.average([x['report']['macro avg']['support'] for x in model[technique]]),
                 },
                 'weighted avg' : {
-                    'precision': np.average([x['report']['weighted avg']['precision'] for x in report]),
-                    'recall' : np.average([x['report']['weighted avg']['recall'] for x in report]),
-                    'f1-score' : np.average([x['report']['weighted avg']['f1-score'] for x in report]),
-                    'support' : np.average([x['report']['weighted avg']['support'] for x in report]),
+                    'precision': np.average([x['report']['weighted avg']['precision'] for x in model[technique]]),
+                    'recall' : np.average([x['report']['weighted avg']['recall'] for x in model[technique]]),
+                    'f1-score' : np.average([x['report']['weighted avg']['f1-score'] for x in model[technique]]),
+                    'support' : np.average([x['report']['weighted avg']['support'] for x in model[technique]]),
                 }
                 }
             })
@@ -65,65 +63,30 @@ def plot_classification_reports_averages(reports, title):
     sns.reset_orig()
     sns.set_theme()
 
-    sns.heatmap(pd.DataFrame(avg_reports[0]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[0][0], xticklabels=False, yticklabels=True, cbar=True).set_title(avg_reports[0]['label'])
-    sns.heatmap(pd.DataFrame(avg_reports[1]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[0][1], xticklabels=False, yticklabels=False, cbar=True).set_title(avg_reports[1]['label'])
-    sns.heatmap(pd.DataFrame(avg_reports[2]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[1][0], xticklabels=True, yticklabels=True, cbar=True).set_title(avg_reports[2]['label'])
-    sns.heatmap(pd.DataFrame(avg_reports[3]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[1][1], xticklabels=True, yticklabels=False, cbar=True).set_title(avg_reports[3]['label'])
-    plt.suptitle(title)
+    # for report, ax in zip(avg_reports, axes):
+    sns.heatmap(pd.DataFrame(avg_reports[0]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[0][0], xticklabels=False, yticklabels=True, cbar=True).set_title(avg_reports[0]['sampling'])
+    sns.heatmap(pd.DataFrame(avg_reports[1]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[0][1], xticklabels=False, yticklabels=False, cbar=True).set_title(avg_reports[1]['sampling'])
+    sns.heatmap(pd.DataFrame(avg_reports[2]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[1][0], xticklabels=True, yticklabels=True, cbar=True).set_title(avg_reports[2]['sampling'])
+    sns.heatmap(pd.DataFrame(avg_reports[3]['report']).iloc[:-1, :].T, annot=True, vmin=0, vmax=1, ax=axes[1][1], xticklabels=True, yticklabels=False, cbar=True).set_title(avg_reports[3]['sampling'])
+    plt.suptitle(f'{name} k-fold average classification report')
     plt.show()
 
 
-def plot_confusion_matrices(models, title):
-    cf_matrices = [confusion_matrix(model['true'], torch.round(torch.sigmoid(torch.tensor(model['preds'])))) for model in models]
+def plot_confusion_matrices(models, name):
+    cf_matrices = [confusion_matrix(model['true'], model['preds']) for model in models.values()]
 
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 6))
 
     sns.reset_orig()
     sns.set_theme()
 
-    sns.heatmap(pd.DataFrame(cf_matrices[0]), fmt='g', annot=True, ax=axes[0][0], xticklabels=False, yticklabels=True, cbar=True, linewidths=0.5, linecolor='black').set_title(models[0]['label'])
-    sns.heatmap(pd.DataFrame(cf_matrices[1]), fmt='g', annot=True, ax=axes[0][1], xticklabels=False, yticklabels=False, cbar=True, linewidths=0.5, linecolor='black').set_title(models[1]['label'])
-    sns.heatmap(pd.DataFrame(cf_matrices[2]), fmt='g', annot=True, ax=axes[1][0], xticklabels=True, yticklabels=True, cbar=True, linewidths=0.5, linecolor='black').set_title(models[2]['label'])
-    sns.heatmap(pd.DataFrame(cf_matrices[3]), fmt='g', annot=True, ax=axes[1][1], xticklabels=True, yticklabels=False, cbar=True, linewidths=0.5, linecolor='black').set_title(models[3]['label'])
+    labels = [name for name in models]
+
+
+    sns.heatmap(pd.DataFrame(cf_matrices[0]), fmt='g', annot=True, ax=axes[0][0], xticklabels=False, yticklabels=True, cbar=True, linewidths=0.5, linecolor='black').set_title(labels[0])
+    sns.heatmap(pd.DataFrame(cf_matrices[1]), fmt='g', annot=True, ax=axes[0][1], xticklabels=False, yticklabels=False, cbar=True, linewidths=0.5, linecolor='black').set_title(labels[1])
+    sns.heatmap(pd.DataFrame(cf_matrices[2]), fmt='g', annot=True, ax=axes[1][0], xticklabels=True, yticklabels=True, cbar=True, linewidths=0.5, linecolor='black').set_title(labels[2])
+    sns.heatmap(pd.DataFrame(cf_matrices[3]), fmt='g', annot=True, ax=axes[1][1], xticklabels=True, yticklabels=False, cbar=True, linewidths=0.5, linecolor='black').set_title(labels[3])
     
-    plt.suptitle(title)
+    plt.suptitle(f'{name} - Confusion Matrices (best f1 score from 5-folds)')
     plt.show()
-
-
-
-'''
-# fig, ax = plt.subplots()
-# ax.plot(range(len(x_points)), y_points, 'r-')
-
-# ax.errorbar(range(len(x_points)), y_points, xerr=0, yerr=error_bars, ecolor = 'b',fmt='ro')
-# ax.set_xticks(range(len(x_points)), [str(x) for x in x_points])
-# ax.set_ylim(-0.1, 1.1)
-# plt.show()
-'''
-
-
-
-'''
-# error_bars_min = []
-# error_bars_max = []
-# x_points = []
-# y_points = []
-
-# for ratio in balance_ratios:
-#     scores = []
-#     for item in results_nn:
-#         if item['ratio'] == ratio:
-#             # scores.append(item['classification_report']['accuracy'])
-#             scores.append(item['accuracy'])
-    
-#     x_points.append(ratio)
-#     y_points.append(np.average(scores))
-#     error_bars_min.append(min(scores))
-#     error_bars_max.append(max(scores))
-
-# error_bars = [[],[]]
-
-# for min_p, max_p, center in zip(error_bars_min, error_bars_max, y_points):
-#     error_bars[0].append(center - min_p)
-#     error_bars[1].append(max_p - center)
-'''
